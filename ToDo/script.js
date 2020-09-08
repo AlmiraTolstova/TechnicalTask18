c853a7268e5f1e94ea6bc61d517a01c9bd1d5940
AlmiraTolstova <almiratolstova@gmail.com>
2020-09-08 10:45:36 +0300

    18-2 change

--- ToDo/script.js
+++ ToDo/script.js
@@ -2,20 +2,22 @@
 // Написать два варианта решения доработки данного кода для вывода i последовательно 0 1 2 3 
 
 // Вариант 1 поменять var на let(видимость в условиях всего блока)
-// const arr = [10, 12, 15, 21];
-// for (let i = 0; i < arr.length; i++) { 
-//     setTimeout(function() {
-//         console.log('Index: ' + i + ', element: ' + arr[i]);
-//                             }, 3000);
-// }
+const arr = [10, 12, 15, 21];
+for (let i = 0; i < arr.length; i++) 
+{ 
+    setTimeout(function() {
+        console.log('Index: ' + i + ', element: ' + arr[i]);
+                            }, 3000);
+}
 
 //Вариант 2
-const arr = [10, 12, 15, 21];
-function logout(i)
-    {
-        console.log('Index: ' + i + ', element: ' + arr[i]);
-    }
-for (var i = 0; i < arr.length; i++) 
+const arr1 = [10, 12, 15, 21];
+function logout(e)
+{
+    console.log('Index: ' + e + ', element: ' + arr[e]);
+}
+
+for (var e = 0; e < arr1.length; e++) 
 { 
-    setTimeout(logout(i), 3000);
+    setTimeout(logout, 3000, e);
 }
