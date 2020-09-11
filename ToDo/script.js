ad4c40046a4f4edbc47da5b0725e12bf701459f4
AlmiraTolstova <almiratolstova@gmail.com>
2020-09-11 11:51:29 +0300

    18-3 change of decision

--- ToDo/script.js
+++ ToDo/script.js
@@ -3,22 +3,19 @@
 
 // Вариант 1 поменять var на let(видимость в условиях всего блока)
 const arr = [10, 12, 15, 21];
-for (let i = 0; i < arr.length; i++) 
-    { 
-        setTimeout(function() {
+for (let i = 0; i < arr.length; i++) {
+    setTimeout(function () {
         console.log('Index: ' + i + ', element: ' + arr[i]);
-        }, 3000);
-    }
+    }, 3000);
+}
 
 //Вариант 2
 const arr1 = [10, 12, 15, 21];
-function logout(e)
-{
+function logout(e) {
     console.log('Index: ' + e + ', element: ' + arr1[e]);
 }
 
-for (var e = 0; e < arr1.length; e++) 
-{ 
+for (var e = 0; e < arr1.length; e++) {
     setTimeout(logout, 3000, e);
 }
 
